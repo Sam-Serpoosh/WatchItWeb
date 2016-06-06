654a7686c69d2dde3de5015bc3c5242cbaf19a18
Sam Serpoosh <saserpoosh@groupon.com>
2016-06-05 23:56:10 -0500

    Calc total paid per category

--- src/Payment.elm
+++ src/Payment.elm
@@ -1,38 +1,86 @@
 module Payment exposing (..)
 
-import String
-import List as L
+import String     as S
+import List       as L
+import List.Extra as LE
 import Util
 
-type alias Payment = { value : Float, category: String, description: String }
+type alias Payment = { value : Float
+                     , category: String
+                     , description: String
+                     }
 
-defaultPayment = { value = 0.0, category = "food", description = "default" }
+defaultPayment : Payment
+defaultPayment = { value       = 0.0
+                 , category    = "food"
+                 , description = "default" 
+                 }
+
+createPayments :: List String -> List Payment
+createPayments paymentsStr = L.map createPayment paymentsStr
 
 createPayment : String -> Payment
-createPayment str = let parts = String.split "," str
-                    in case parts of
-                      []                  -> defaultPayment
-                      _ :: []             -> defaultPayment
-                      _ :: _ :: []        -> defaultPayment
-                      (val::cat::desc::_) -> {
-                                               value       = Util.toFloatSure val,
-                                               category    = cat,
-                                               description = desc
-                                             }
+createPayment str =
+  let parts = S.split "," str
+  in case parts of
+    []                  -> defaultPayment
+    _ :: []             -> defaultPayment
+    _ :: _ :: []        -> defaultPayment
+    (val::cat::desc::_) -> { value       = Util.ensureToFloat val
+                           , category    = cat
+                           , description = desc
+                           }
   
 totalPaid : List Payment -> Float
-totalPaid payments = L.sum (L.map .value payments)
+totalPaid payments =
+  L.sum (L.map .value payments)
+
+paymentsByCategory : List Payment -> List (String, List Payment)
+paymentsByCategory payments =
+  let grouped = LE.groupWhile (\p1 p2 -> p1.category == p2.category) (L.sortBy .category payments)
+  in L.map (\ps -> (ps |> Util.ensureHead defaultPayment |> .category, ps)) grouped
+
+paidPerCategory : List (String, List Payment) -> List (String, Float)
+paidPerCategory = L.map (\(cat, payments) -> (cat, payments |> totalPaid))
 
 -- Tests for Payment
 
 testCreatePayment : String -> Payment -> Bool
-testCreatePayment str payment = (createPayment str) == payment
+testCreatePayment str payment =
+  (createPayment str) == payment
 
 testTotalPaid : List Payment -> Float -> Bool
-testTotalPaid payments expectedPaid = (totalPaid payments) == expectedPaid
+testTotalPaid payments expectedPaid =
+  (totalPaid payments) == expectedPaid
+
+testPaymentsByCategory : List Payment -> List (String, List Payment) -> Bool
+testPaymentsByCategory payments expectedGrouped =
+  (paymentsByCategory payments) == expectedGrouped
+
+testPaidPerCategory : List (String, List Payment) -> List (String, Float) -> Bool
+testPaidPerCategory paymentsByCat expectedPaidPerCat =
+  (paidPerCategory paymentsByCat) == expectedPaidPerCat
+
+expectedGrouped : List (String, List Payment)
+expectedGrouped = [("food", [lunch, snack]), ("transportation", [uber])]
 
 samplePayments : List Payment
-samplePayments = [
-                   { value = 10.0, category = "food"           , description = "lunch"},
-                   { value = 25.0, category = "trannsportation", description = "uber"}
-                 ]
+samplePayments = [lunch, uber, snack]
+
+lunch : Payment
+lunch = { value       = 10.0
+        , category    = "food"
+        , description = "lunch"
+        }
+
+snack : Payment
+snack  = { value = 5.0
+         , category = "food"
+         , description = "snack"
+         }
+
+uber : Payment
+uber = { value    = 25.0
+       , category = "transportation"
+       , description = "uber"
+       }
--- src/Util.elm
+++ src/Util.elm
@@ -1,9 +1,18 @@
 module Util exposing (..)
 
 import String as S
+import List   as L
 
-toFloatSure : String -> Float
-toFloatSure str = let f = S.toFloat str
-                  in case f of
-                    Ok num -> num
-                    Err _  -> 0.0
+ensureToFloat : String -> Float
+ensureToFloat str =
+  let f = S.toFloat str
+  in case f of
+    Ok num -> num
+    Err _  -> 0.0
+
+ensureHead : a -> List a -> a
+ensureHead default items =
+  let head = items |> L.head
+  in case head of
+    Just elem -> elem
+    Nothing   -> default
