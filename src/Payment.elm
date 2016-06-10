d438bc73a0081b6c225734143f18fc20693ec9db
Sam Serpoosh <saserpoosh@groupon.com>
2016-06-10 00:02:52 -0500

    Add documentation for exposed functions of Payment

--- src/Payment.elm
+++ src/Payment.elm
@@ -1,4 +1,20 @@
-module Payment exposing (..)
+module Payment exposing (
+  createPayments,
+  totalPaid,
+  paymentsByCategory,
+  paidPerCategory,
+  percentPaidPerCategory)
+
+{-| This module does some basic calculations and stats
+on a list of Payments and return some reports.
+
+# Creation of Payment
+@docs createPayments
+
+# Calculations
+@docs totalPaid, paymentsByCategory, paidPerCategory, percentPaidPerCategory
+
+-}
 
 import String     as S
 import List       as L
@@ -15,9 +31,51 @@ defaultPayment = { value       = 0.0
                  , category    = "food"
                  , description = "default" 
                  }
+{-| create a Payment from a list of CSV Strings.
+
+    createPayments ["10.00,food,lunch", "25.00,transportation,uber"] == [p1, p2]
+-}
 
 createPayments : List String -> List Payment
 createPayments paymentsStr = L.map createPayment paymentsStr
+
+
+{-| Calculates total amound paid.
+
+    totalPaid [p1-10, p2-5] == 15
+-}
+
+totalPaid : List Payment -> Float
+totalPaid payments =
+  L.sum (L.map .value payments)
+
+{-| categorize payments by their category value.
+
+    paymentsByCategory [p1-food, p2-transportation, p3-food] == [("food", [p1, p3]), ("transportation", [p2])]
+-}
+
+paymentsByCategory : List Payment -> List (String, List Payment)
+paymentsByCategory payments =
+  let grouped = LE.groupWhile (\p1 p2 -> p1.category == p2.category) (L.sortBy .category payments)
+  in L.map (\ps -> (ps |> Util.ensureHead defaultPayment |> .category, ps)) grouped
+
+{-| Calculates total amound paid per category.
+
+    totalPaid [p1-food-10, p2-transportation-5, p3-food-20] == [("food", 30), ("transportation", 5)]
+-}
+
+paidPerCategory : List (String, List Payment) -> List (String, Float)
+paidPerCategory = L.map (\(cat, payments) -> (cat, payments |> totalPaid))
+
+{-| Calculates percent of total paid for each category.
+
+    totalPaid [p1-food-10, p2-transportation-25, p3-food-5] == [("food", 0.375), ("transportation", 0.625)]
+-}
+
+percentPaidPerCategory : List (String, Float) -> List (String, Float)
+percentPaidPerCategory paidPerCat =
+  let total = L.foldl (\a b -> a + b) 0.0 (L.map snd paidPerCat)
+  in L.map (\(cat, paid) -> (cat, paid / total)) paidPerCat
 
 createPayment : String -> Payment
 createPayment str =
@@ -30,23 +88,6 @@ createPayment str =
                            , category    = cat
                            , description = desc
                            }
-  
-totalPaid : List Payment -> Float
-totalPaid payments =
-  L.sum (L.map .value payments)
-
-paymentsByCategory : List Payment -> List (String, List Payment)
-paymentsByCategory payments =
-  let grouped = LE.groupWhile (\p1 p2 -> p1.category == p2.category) (L.sortBy .category payments)
-  in L.map (\ps -> (ps |> Util.ensureHead defaultPayment |> .category, ps)) grouped
-
-paidPerCategory : List (String, List Payment) -> List (String, Float)
-paidPerCategory = L.map (\(cat, payments) -> (cat, payments |> totalPaid))
-
-percentPaidPerCategory : List (String, Float) -> List (String, Float)
-percentPaidPerCategory paidPerCat =
-  let total = L.foldl (\a b -> a + b) 0.0 (L.map snd paidPerCat)
-  in L.map (\(cat, paid) -> (cat, paid / total)) paidPerCat
 
 -- Tests for Payment
 
